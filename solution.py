8800f2b1cd8fc062c54079774575b1307d5c6b15
Nastya <mail@yuhimenko-anastasia.ru>
2026-04-18 02:03:53 +0300

    fix butches

--- solution.py
+++ solution.py
@@ -5,6 +5,8 @@ import soundfile as sf
 
 
 class PredictionModel:
+    batch_size = 100
+
     def __init__(self) -> None:
         pass
 
@@ -17,7 +19,7 @@ class PredictionModel:
                 mono = audio.mean(axis=1).astype(np.float64)
 
                 score = float(np.percentile(np.abs(mono), 99))
-            except Exception:   # noqa
+            except Exception:  # noqa
                 score = 0.0
             scores.append(score)
         return scores
