b3f267fff810f36698fc3356787229fd7382400b
Nastya <mail@yuhimenko-anastasia.ru>
2026-04-17 19:08:31 +0300

    update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,28 @@
-from pathlib import Path
+# from pathlib import Path
 
+# import numpy as np
+# import soundfile as sf
+
+
+# class PredictionModel:
+#     batch_size: int = 10
+
+#     def __init__(self) -> None:
+#         pass
+
+#     def predict(self, batch: list[Path]) -> list[float]:
+#         """Получает батч путей к WAV-файлам, возвращает список anomaly_score."""
+#         scores = []
+#         for path in batch:
+#             try:
+#                 audio, _ = sf.read(str(path), always_2d=True)
+#                 rms = float(np.sqrt(np.mean(audio.astype(np.float64) ** 2)))
+#             except Exception:
+#                 rms = 0.0
+#             scores.append(rms)
+#         return scores
+
+from pathlib import Path
 import numpy as np
 import soundfile as sf
 
@@ -7,17 +30,19 @@ import soundfile as sf
 class PredictionModel:
     batch_size: int = 10
 
-    def __init__(self) -> None:
+    def init(self) -> None:
         pass
 
     def predict(self, batch: list[Path]) -> list[float]:
-        """Получает батч путей к WAV-файлам, возвращает список anomaly_score."""
         scores = []
         for path in batch:
             try:
                 audio, _ = sf.read(str(path), always_2d=True)
-                rms = float(np.sqrt(np.mean(audio.astype(np.float64) ** 2)))
+                mono = audio.mean(axis=1).astype(np.float64)
+
+                # Топ-5% самых громких моментов
+                score = float(np.percentile(np.abs(mono), 95))
             except Exception:
-                rms = 0.0
-            scores.append(rms)
+                score = 0.0
+            scores.append(score)
         return scores
